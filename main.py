d6fc23b69ace859cdbd07a2630cacd0f7d3f2afc
Parth171 <parthsakpal17@gmail.com>
2023-12-27 22:36:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+
+# IMPORT LIBRARIES
 import openai
 import pyttsx3
 import speech_recognition as sr
-
 from api_key import KEY
-
 
 openai.api_key = KEY
 
-RECOGNIZER = sr.Recognizer()
+
 
 def record_text():
     while True:
@@ -23,13 +23,19 @@ def record_text():
             print("Unknown Value Error")
     return
 
-def ai_therapist():
-    input_text = record_text()
-    prompt = f"""
-    You are an AI therapist named Sukha, which means contentment and peace in Sanskrit. Your job is to speak as if you are a therapist to the user. The user statement is delimited by triple back ticks. Keep your response under 30 words.
 
-    User Statement: ```{input_text}```
-    """
+
+
+def run():
+    INPUT = record_text()
+    prompt = f"""
+                    You are an AI therapist named Sukha, which means contentment and peace in Sanskrit. Your job is to speak as if you are a therapist to the user. The user statement is delimited by triple back ticks. Keep your response under 30 words.
+
+
+                    User Statement: ```{INPUT}```
+
+
+                    """
 
     output = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -38,18 +44,14 @@ def ai_therapist():
 
     ai_text = output.choices[0].message["content"]
 
+
     engine = pyttsx3.init()
     engine.say(ai_text)
     engine.runAndWait()
 
-def start_therapy():
-    ai_therapist()
+if __name__ == "__main__":
 
-# Create GUI
-"""root = tk.Tk()
-root.title("AI Therapist")
+    RECOGNIZER = sr.Recognizer()
 
-start_button = tk.Button(root, text="Start Therapy", command=start_therapy)
-start_button.pack()
-
-root.mainloop()"""
+    while True:
+        run()
